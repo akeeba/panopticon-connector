bc7b37263657aba9c4817207a440d423e6c95192
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-11-21 13:33:54 +0200

    Bump API version

--- build/templates/version.php
+++ build/templates/version.php
@@ -9,4 +9,4 @@ defined('_JEXEC') || die;
 
 const AKEEBA_PANOPTICON_VERSION = '##VERSION##';
 const AKEEBA_PANOPTICON_DATE    = '##DATE##';
-const AKEEBA_PANOPTICON_API     = 100;
+const AKEEBA_PANOPTICON_API     = 101;
